df90201ab13343ca48cfbde6d9ea8a5ad99461fb
codergalib20 <coderboygalif@gmail.com>
2021-11-30 11:51:22 +0600

    commited my app

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dpacy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-console.log(uri);
+
+
 async function run() {
   try {
     await client.connect();
     console.log("database connected successfully");
     const database = client.db("online_foods");
     const foodCollection = database.collection("foods");
+    const orderCollection = database.collection("orders");
     //  Get All Foods__
     app.get("/foods", async (req, res) => {
       const cursor = foodCollection.find({});
@@ -34,6 +37,22 @@ async function run() {
       const food = await foodCollection.findOne(query);
       res.json(food);
     });
+
+    // Get All Orders__
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+
+    // Order All Orders__
+    app.post('/orders', async(req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+      });
+   
+
   } finally {
     // await client.close();
   }
